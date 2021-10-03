4439bf5dc9c22ccd1f2f2cd109a81d3b560204c1
JensWillems <71693119+JensWillems@users.noreply.github.com>
2021-10-03 11:57:25 +0200

    Exercise 5

--- S1/index.js
+++ S1/index.js
@@ -7,14 +7,36 @@ window.onload = function () {
         event.preventDefault();
         // console.log("test");
 
-        const name = document.getElementById('nameInput').value;
-        const mail = document.getElementById('emailInput').value;
-        const order = document.getElementById('orderInput').value;
-
-        document.getElementById('message').innerHTML = getMessage(name, mail, order);
+        let orderDetails = {
+            name: document.getElementById('nameInput').value,
+            mail: document.getElementById('emailInput').value,
+            order: document.getElementById('orderInput').value
+        }
+        document.getElementById('message').innerHTML = printOrder(orderDetails);
     });
 
-    function getMessage(name, mail, order) {
-        return `<p>The order for the customer ${name} is the following:  ${order}. The customer may be notified by email:  ${mail}</p>`
+    function printOrder(orderDetails) {
+        return `<p>The order for the customer ${orderDetails.name} is the following:  ${orderDetails.order}. The customer may be notified by email:  ${orderDetails.mail}</p>`
     };
+
 };
+
+
+// window.onload = function () {
+//     console.log('Loaded');
+
+//     document.getElementById('form').addEventListener("submit", event => {
+//         event.preventDefault();
+//         // console.log("test");
+
+//         const name = document.getElementById('nameInput').value;
+//         const mail = document.getElementById('emailInput').value;
+//         const order = document.getElementById('orderInput').value;
+
+//         document.getElementById('message').innerHTML = getMessage(name, mail, order);
+//     });
+
+//     function getMessage(name, mail, order) {
+//         return `<p>The order for the customer ${name} is the following:  ${order}. The customer may be notified by email:  ${mail}</p>`
+//     };
+// };
