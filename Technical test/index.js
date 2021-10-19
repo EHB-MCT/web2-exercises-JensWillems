0a5e73437639ce4016dfa780bc1548da063ac421
JensWillems <71693119+JensWillems@users.noreply.github.com>
2021-10-19 09:20:56 +0200

    technical test api WIP

--- Technical test/index.js
+++ Technical test/index.js
@@ -1,5 +1,30 @@
 "use strict";
+"use strict";
 
+//     console.log("test");
+//     const options = {
+//         method: 'GET',
+//         mode: 'no-cors',
+//         haeders: {
+//             //header('Access-Control-Allow-Origin: *');
+//             'Access-Control-Allow-Origin': 'http://example.com',
+//         }
+//         // headers: {
+//         //   'Authorization': `Bearer https://cartes.io/`,
+//         // }
+//       };
+// window.onload = function() {
+//     fetch('https://cartes.io/api/maps/a61bce50-20be-4b31-a7ee-cfaa31325813',options)
+//         .then(response => {
+//              console.log("test1");
+//              response.json()
+//         })
+//         .then(data => 
+//             {
+//                 console.log("test2")
+//                 console.log(data)});
+
+// };
 fetch('https://cartes.io/api/maps/048eebe4-8dac-46e2-a947-50b6b8062fec')
     .then(response => response.json())
     .then(data => console.log(data));
